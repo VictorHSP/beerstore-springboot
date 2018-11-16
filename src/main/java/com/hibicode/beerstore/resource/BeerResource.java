e8b6fa853543335d11aa4694fcda2dc773276317
VictorHSP <victor.hugo.s@hotmail.com>
2018-11-15 22:27:53 -0200

    chg: Alterando Http response ao salvar cerveja para HTTP.CREATED 201

--- src/main/java/com/hibicode/beerstore/resource/BeerResource.java
+++ src/main/java/com/hibicode/beerstore/resource/BeerResource.java
@@ -3,6 +3,7 @@ package com.hibicode.beerstore.resource;
 import com.hibicode.beerstore.model.Beer;
 import com.hibicode.beerstore.repository.Beers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -21,6 +22,7 @@ public class BeerResource {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Beer create(@RequestBody Beer beer) {
         return beers.save(beer);
     }
