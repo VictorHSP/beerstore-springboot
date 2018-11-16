23d7e367dc9c761c2143a983b0f63b0f97295968
VictorHSP <victor.hugo.s@hotmail.com>
2018-11-16 21:47:26 -0200

    new: adicionando dependencia do mockito e criando query method para buscar cerveja pelo nome e tipo.

--- src/main/java/com/hibicode/beerstore/service/BeerService.java
+++ src/main/java/com/hibicode/beerstore/service/BeerService.java
@@ -1,12 +1,29 @@
 package com.hibicode.beerstore.service;
 
 import com.hibicode.beerstore.model.Beer;
+import com.hibicode.beerstore.repository.Beers;
 import com.hibicode.beerstore.service.exception.BeerAlreadyExistException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class BeerService {
 
+    @Autowired
+    private Beers beers;
+
     public Beer save(final Beer beer) {
-        throw new BeerAlreadyExistException();
+
+        Optional<Beer> beerByNameAndType = beers.findByNameAndType(beer.getName(), beer.getType());
+
+        if (beerByNameAndType.isPresent()) {
+            throw new BeerAlreadyExistException();
+        }
+
+        return beers.save(beer);
+
     }
 
 }
