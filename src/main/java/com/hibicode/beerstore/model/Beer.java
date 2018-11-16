4033fc35986a52c785e3ddd4b6196091609e422f
VictorHSP <victor.hugo.s@hotmail.com>
2018-11-15 23:11:40 -0200

    new: Adicionando codigos em cada validação feita pelo Bean Validation

--- src/main/java/com/hibicode/beerstore/model/Beer.java
+++ src/main/java/com/hibicode/beerstore/model/Beer.java
@@ -20,14 +20,14 @@ public class Beer {
     @EqualsAndHashCode.Include
     private Long id;
 
-    @NotBlank
+    @NotBlank(message = "beers-1")
     private String name;
 
-    @NotNull
+    @NotNull(message = "beers-2")
     private BeerType type;
 
-    @NotNull
-    @DecimalMin(value = "0")
+    @NotNull(message = "beers-3")
+    @DecimalMin(value = "0", message = "beers-4")
     private BigDecimal volume;
 
 
