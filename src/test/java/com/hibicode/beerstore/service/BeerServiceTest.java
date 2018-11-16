ea819cf92bbce601985356411f67b8bb351e78e7
VictorHSP <victor.hugo.s@hotmail.com>
2018-11-16 21:36:52 -0200

    new: Criando as assertivas para salvar cerveja.

--- src/test/java/com/hibicode/beerstore/service/BeerServiceTest.java
+++ src/test/java/com/hibicode/beerstore/service/BeerServiceTest.java
@@ -3,6 +3,11 @@ package com.hibicode.beerstore.service;
 import com.hibicode.beerstore.model.Beer;
 import com.hibicode.beerstore.model.BeerType;
 import com.hibicode.beerstore.service.exception.BeerAlreadyExistException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -11,16 +16,35 @@ public class BeerServiceTest {
 
     private BeerService beerService;
 
+    @Before
+    public void setup() {
+        beerService = new BeerService();
+    }
+
+
     @Test(expected = BeerAlreadyExistException.class)
     public void should_deny_creation_of_beer_that_exists() {
-        beerService = new BeerService();
-
         Beer newBeer = new Beer();
         newBeer.setName("Heineken");
         newBeer.setType(BeerType.LAGER);
         newBeer.setVolume(new BigDecimal("350"));
 
         beerService.save(newBeer);
+    }
+
+    @Test
+    public void should_create_new_beer() {
+        Beer newBeer = new Beer();
+        newBeer.setName("Heineken");
+        newBeer.setType(BeerType.LAGER);
+        newBeer.setVolume(new BigDecimal("350"));
+
+        Beer beerSaved = beerService.save(newBeer);
+
+        assertThat(beerSaved.getId(), equalTo(10L));
+        assertThat(beerSaved.getName(), equalTo("Heineken"));
+        assertThat(beerSaved.getType(), equalTo(BeerType.LAGER));
+
     }
 
 }
