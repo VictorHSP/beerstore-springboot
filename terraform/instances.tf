f3d6e992d9f3db4884e56962c488edf41fd9e6b8
VictorHSP <victor.hugo.s@hotmail.com>
2018-11-18 20:24:36 -0200

    new: Script para automatizar a criação da infra e busca do IP para liberar acesso.

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -12,11 +12,28 @@ resource "aws_instance" "instances" {
 
   key_name = "${aws_key_pair.key_pair.key_name}"
 
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}",
+    "${aws_security_group.allow_outbound.id}"]
 
   tags {
     Name = "hibicode_instances"
   }
+}
+
+// Busca os ips publicos e preenche na variavel
+data "template_file" "hosts" {
+  template = "${file("./template/hosts.tpl")}"
+  vars {
+    PUBLIC_IP_0 = "${aws_instance.instances.*.public_ip[0]}"
+    PUBLIC_IP_1 = "${aws_instance.instances.*.public_ip[1]}"
+    PUBLIC_IP_2 = "${aws_instance.instances.*.public_ip[2]}"
+  }
+}
+
+// Apos buscar preenche no arquivo hosts.tpl
+resource "local_file" "hosts" {
+  content = "${data.template_file.hosts.rendered}"
+  filename = "./hosts"
 }
 
 output "public_ips" {
--- terraform/security.tf
+++ terraform/security.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["187.20.21.133/32"]
+    cidr_blocks = ["${var.my_public_ip}"]
   }
 }
 
@@ -19,5 +19,19 @@ resource "aws_security_group" "database" {
     to_port = 5432
     protocol = "tcp"
     self = true // Libera a conexão quem estiver com esse sercurity group configurado
+  }
+}
+
+
+resource "aws_security_group" "allow_outbound" {
+
+  vpc_id = "${aws_vpc.main.id}"
+  name = "hibicode_allow_outbound"
+
+  egress {
+    from_port = 0 // qualquer porta
+    to_port = 0
+    protocol = "-1" // qualquer protocolo
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
